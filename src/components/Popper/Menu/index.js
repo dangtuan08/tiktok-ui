b324cc104e5cacc8bb69fc2f4c1d521edff5f3bf
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2022-05-12 00:04:09 +0700

    Bài 67: Xây dựng UI phần Header #4 phần 2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,44 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 
-
 import { Wrapper as PopperWrapper } from '~/components/Popper';
-import styles from './Menu.module.scss'
-import Button from '~/components/Button';
-
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
 
 const cx = classNames.bind(styles);
 
 function Menu({ children, items = [] }) {
-
     const renderItems = () => {
-
         return items.map((item, index) => {
-            return <Button key={index} leftIcon={item.icon}>{item.title}</Button>
-        })
-
-    }
+            // console.log(item);
+            return <MenuItem key={index} data={item} />;
+        });
+        // return items.map((item, index) => {
+        //     return <Button key={index} leftIcon={item.icon}>{item.title}</Button>
+        // })
+    };
 
     return (
         <Tippy
             interactive
             delay={[0, 800]}
-
-            // visible
-            placement='bottom-end'
+            visible
+            placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        {renderItems()}
-                    </PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
                 </div>
             )}
         >
-
             {children}
         </Tippy>
-    )
+    );
 }
 
-export default Menu
+export default Menu;
