8fecf1c26f82745325b4363cc8260a219884cc6c
dangtuan08 <48054696+dangtuan08@users.noreply.github.com>
2022-06-16 11:48:22 +0700

    Bài 115: Xây dựng UI phần sidebar

---
+++ src/pages/Live/Live.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Live = (props) => {
+    return <div>Live</div>;
+};
+
+Live.propTypes = {};
+
+export default Live;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,11 +7,13 @@ import Profile from '~/pages/Profile';
 import Search from '~/pages/Search';
 import Upload from '~/pages/Upload';
 import config from '~/config';
+import Live from '~/pages/Live';
 //Public route
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
     { path: config.routes.profile, component: Profile },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
 ];
